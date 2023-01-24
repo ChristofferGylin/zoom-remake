b27e7e976640e4385417c631285965c7a7fef2af
Christoffer <christoffer@christoffergylin.com>
2023-01-24 12:30:14 +0100

    Fixed hover effect for mobile menu

--- src/MobileMenu.jsx
+++ src/MobileMenu.jsx
@@ -8,10 +8,10 @@ const MobileMenu = () => {
         <div id="mobileMenuContainer" className='w-full md:hidden text-base'>
             <div id="mobileMenu" className='h-screen w-full bg-slate-50 hidden'>
                 <ul className='flex flex-col justify-start space-y-4 w-4/5 mx-auto mt-8'>
-                    <li className='flex w-full'> <BiSearch /><a href="#">Search</a></li>
-                    <li className='flex justify-between items-center w-full border-b border-slate-300 py-4 hover:text-zoom-blue hover:cursor-pointer'><button>Products</button><IoIosArrowDown className='text-zoom-blue' /></li>
-                    <li className='flex justify-between items-center w-full border-b border-slate-300 py-4 hover:text-zoom-blue hover:cursor-pointer'><button>Solutions</button><IoIosArrowDown className='text-zoom-blue' /></li>
-                    <li className='flex justify-between items-center w-full border-b border-slate-300 py-4 hover:text-zoom-blue hover:cursor-pointer'><button>Resources</button><IoIosArrowDown className='text-zoom-blue' /></li>
+                    <li className='flex w-full hover:text-zoom-blue hover:cursor-pointer'> <BiSearch /><a href="#">Search</a></li>
+                    <li className='group flex justify-between items-center w-full border-b border-slate-300 py-4'><button className='group-hover:text-zoom-blue group-hover:cursor-pointer'>Products</button><IoIosArrowDown className='group-hover:text-zoom-blue group-hover:cursor-pointer' /></li>
+                    <li className='group flex justify-between items-center w-full border-b border-slate-300 py-4'><button className='group-hover:text-zoom-blue group-hover:cursor-pointer'>Solutions</button><IoIosArrowDown className='group-hover:text-zoom-blue group-hover:cursor-pointer' /></li>
+                    <li className='group flex justify-between items-center w-full border-b border-slate-300 py-4'><button className='group-hover:text-zoom-blue group-hover:cursor-pointer'>Resources</button><IoIosArrowDown className='group-hover:text-zoom-blue group-hover:cursor-pointer' /></li>
                     <li className='hover:text-zoom-blue'><a href="">Plans & Pricing</a></li>
                 </ul>
             </div>
